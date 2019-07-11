38e93aa4cf77d2d3d22109d7489fb439518dc0bd
Pat Haynes <pathaynes32@gmail.com>
2019-07-10 19:56:02 -0700

    simple hello world app

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+// Reference neede elements
+const result = document.getElementById('result');
+const nameInput = document.getElementById('name');
+
+// Action to take on user event
+function updateResult() {
+    result.textContent = nameInput.value;
+} 
